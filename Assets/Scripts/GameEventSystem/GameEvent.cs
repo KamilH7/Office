7555cfff6ebe8f705b6f988cf52edbc181843640
Kamil Haraf <kamilharaf7@gmail.com>
2022-05-20 15:05:14 +0200

    Added abstract game events

---
+++ Assets/Scripts/GameEventSystem/GameEvent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameEventSystem
+{
+    public abstract class GameEvent : ScriptableObject
+    {
+        #region Events
+
+        private event Action OnInvoke;
+
+        #endregion
+
+        #region Public Methods
+
+        public void Subscribe(Action action)
+        {
+            OnInvoke += action;
+        }
+
+        public void UnSubscribe(Action action)
+        {
+            OnInvoke -= action;
+        }
+
+        public virtual void Invoke()
+        {
+            OnInvoke?.Invoke();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/GameEventSystem/GameEventWithParameter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameEventSystem
+{
+    public abstract class GameEventWithParameter<T> : ScriptableObject
+    {
+        #region Events
+
+        private event Action<T> OnInvoke;
+
+        #endregion
+
+        #region Public Methods
+        
+        
+        public void Subscribe(Action<T> action)
+        {
+            OnInvoke += action;
+        }
+        
+        public void UnSubscribe(Action<T> action)
+        {
+            OnInvoke -= action;
+        }
+        
+        public virtual void Invoke(T parameter)
+        {
+            OnInvoke?.Invoke(parameter);
+        }
+
+        #endregion
+    }
+}
