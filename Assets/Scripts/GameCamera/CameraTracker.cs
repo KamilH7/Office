83b3e65eb9419768cf0dae6e42fd897f4451d8ba
Kamil Haraf <kamilharaf7@gmail.com>
2022-05-21 16:02:18 +0200

    Added camera data SO

---
+++ Assets/Scripts/GameCamera/CameraTracker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace GameCamera
+{
+    public class CameraTracker : MonoBehaviour
+    {
+        [SerializeField]
+        private GameCameraData gameCameraData;
+        [SerializeField]
+        private Transform gameCameraTransform;
+        private void Update()
+        {
+            gameCameraData.UpdateCameraPointingDirection(gameCameraTransform.forward);
+        }
+    }
+}
---
+++ Assets/Scripts/GameCamera/GameCameraData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace GameCamera
+{
+    [CreateAssetMenu(fileName = "CameraData", menuName = "SO/Camera/CameraData")]
+    public class GameCameraData : ScriptableObject
+    {
+        #region Public Properties
+
+        public Vector3 CameraPointingDirection { get; private set; }
+
+        #endregion
+
+        #region Public Methods
+
+        public void UpdateCameraPointingDirection(Vector3 newPointingDirection)
+        {
+            CameraPointingDirection = newPointingDirection;
+        }
+
+        #endregion
+    }
+}
